887af6bc8a21a7f41c4d50bc913e3aa4d2d320c8
Abdul Motin <motin.mmk.jr@gmail.com>
2023-03-27 14:18:38 +0600

    crud

--- app.js
+++ app.js
@@ -51,7 +51,59 @@ app.get('/addpost1', (req, res) => {
     });
 });
 
+//INSERT DATA INTO TABLE
+app.get('/addpost2', (req, res) => {
+    let post = { title: 'Post Two', body: 'This is post number Two' };
+    let sql = 'INSERT INTO post SET ?';
+    db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    });
+});
+
+
+//get all post
+app.get('/getpost', (req, res) => {
+    let sql = 'SELECT * FROM post';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts fetched...');
+    });
+});
+
+// get single post
+app.get('/getpost/:id', (req, res) => {
+    let sql = `SELECT * FROM post WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts fetched...');
+    });
+});
+
+//update post
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `UPDATE post SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post updated...');
+    });
+});
+
+//delete post
+app.get('/deletepost/:id', (req, res) => {
+    let sql = `DELETE FROM post WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post deleted...');
+    });
+});
 
 app.listen('3000', () => {
-    console.log('app runnig on Port 3000')
+    console.log('app runnig on Port 3000');
 });
